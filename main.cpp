871bbc405c8131ff3ab24a5f290bd5fdaade3dc2
Magdi Eissa <magdieissa@aucegypt.edu>
2026-04-14 14:23:30 +0200

    Add menu with signup login logout using map

--- main.cpp
+++ main.cpp
@@ -1,22 +1,78 @@
 #include <iostream>
+#include <map>
+#include <string>
 #include "utils.hpp"
 
+using namespace std;
+
 int main() {
-    std::string email;
+    map<string, string> users;
+    bool logged_in = false;
+    string current_user = "";
+    int choice;
 
     while (true) {
-        std::cout << "Enter your email: ";
-        std::cin >> email;
+        cout << "\nMenu:\n";
 
-        try {
-            check_email(email);
-            std::cout << "Valid email entered.\n";
+        if (!logged_in) {
+            cout << "1. Sign up\n";
+            cout << "2. Login\n";
+        } else {
+            cout << "3. Logout\n";
+        }
+
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (!logged_in && choice == 1) {
+            string email, password;
+
+            cout << "Enter your email: ";
+            cin >> email;
+
+            try {
+                check_email(email);
+            } catch (const invalid_argument& e) {
+                cout << e.what() << "\n";
+                continue;
+            }
+
+            cout << "Enter your password: ";
+            cin >> password;
+
+            users[email] = password;
+            cout << "Sign up successful!\n";
+        }
+        else if (!logged_in && choice == 2) {
+            string email, password;
+
+            cout << "Enter your email: ";
+            cin >> email;
+            cout << "Enter your password: ";
+            cin >> password;
+
+            if (users.find(email) != users.end() && users[email] == password) {
+                logged_in = true;
+                current_user = email;
+                cout << "Login successful!\n";
+            } else {
+                cout << "Invalid email or password.\n";
+            }
+        }
+        else if (logged_in && choice == 3) {
+            logged_in = false;
+            current_user = "";
+            cout << "Logged out successfully!\n";
+        }
+        else if (choice == 0) {
+            cout << "Exiting program.\n";
             break;
-        } catch (const std::invalid_argument& e) {
-            std::cout << e.what() << " Try again.\n";
+        }
+        else {
+            cout << "Invalid option.\n";
         }
     }
 
     return 0;
 }
-
